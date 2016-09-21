26badc74b85e879d6c17e662ebd3c9b700eaee18
Jhonatan <jopa_1293@hotmail.com>
2016-09-20 21:15:40 -0500

    trabajo telematica control de fallos

---
+++ LRC/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace LRC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDivisor = new System.Windows.Forms.TextBox();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.lblResul = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(35, 61);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(169, 20);
+            this.txtDato.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDivisor
+            // 
+            this.txtDivisor.Location = new System.Drawing.Point(210, 61);
+            this.txtDivisor.Name = "txtDivisor";
+            this.txtDivisor.Size = new System.Drawing.Size(62, 20);
+            this.txtDivisor.TabIndex = 1;
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Location = new System.Drawing.Point(125, 132);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(35, 13);
+            this.lblDatos.TabIndex = 3;
+            this.lblDatos.Text = "label1";
+            // 
+            // lblResul
+            // 
+            this.lblResul.AutoSize = true;
+            this.lblResul.Location = new System.Drawing.Point(125, 156);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(35, 13);
+            this.lblResul.TabIndex = 4;
+            this.lblResul.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 346);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.txtDivisor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDato);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDivisor;
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.Label lblResul;
+    }
+}
+
---
+++ LRC/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LRC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[,] arg;
+        public int contarUltimosBit(string dato,int acumula)
+        {
+            int suma = 0;
+            for (int i = 0; i < dato.Length; i++)
+            {
+                if (acumula <= i)
+                {
+                    suma += 1;
+                }
+            }
+            return suma;
+        }
+        public int contarUnos(int c,int fila)
+        {
+            int contar = 0;
+            for(int f = 0; f < fila; f++)
+            {
+                if (arg[f, c] == 1)
+                {
+                    contar += 1;
+                }
+
+            }
+            return contar;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dato = txtDato.Text;
+            string correcion = "";
+            int divisor = int.Parse(txtDivisor.Text);
+            int fila=0, column = 0;
+            int resul = dato.Length / divisor;
+            int acumula = resul * divisor;//es para hacer la multiplicacion del divisor con el resul
+            //nos sirve para realizar cuantos ceros tengo que agregar a los ultimos espacios 
+            if (dato.Length%divisor==0) {
+                correcion = dato;
+                fila = resul;
+            }
+            else
+            {
+                int addCeros = divisor-contarUltimosBit(dato, acumula);
+                for (int i=0;i<dato.Length;i++) {
+                    if (acumula == i) {
+                        for (int b = 0; b < addCeros; b++) {
+                            correcion += "0";
+                        }
+                    }
+                    correcion += dato[i];
+                }
+                fila = resul + 1;                 
+            }
+            lblDatos.Text = correcion.ToString();
+            column = divisor;
+            lblResul.Text = "";
+            dato = "";
+            int pos = 0;
+            arg = new int[fila, column];
+            for (int f=0;f<fila;f++) {
+                for (int c = 0; c < column; c++) {
+                    arg[f, c] = int.Parse(correcion[pos].ToString());
+                    lblResul.Text += correcion[pos].ToString();
+                    pos += 1;
+                }
+                lblResul.Text += "\n";
+            }
+            lblResul.Text += "\n";
+            int[] redun = new int[column];
+            
+            for (int i = 0; i < column; i++)
+            {
+                if (contarUnos(i, fila) % 2 == 0) {
+                    redun[i] = 0;
+                } else
+                {
+                    redun[i] = 1;
+                }
+                lblResul.Text += redun[i].ToString();
+            }
+            
+        }
+    }
+}
